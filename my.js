088c613d357c762b83e12c1901f2ee21f3690e82
hientrinh286 <hientrinh286@gmail.com>
2021-05-18 20:19:04 +0700

    tinh dien tich va chu vi hinh tron

---
+++ my.js
@@ -0,0 +1,6 @@
+let r = +prompt('Nhập bán kính hình tròn (cm):');
+const pi = 3.14;
+c = 2*pi*r;
+s = pi*r*r;
+document.write("Chu vi của hình tròn là: "+ c+ ' (cm)'+'<br>');
+document.write('Diện tich của hình tròn là: '+s+' (cm2)');
